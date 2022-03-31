c07ca1c053f712c666e09cc97004f4c5dcdcb4e3
bcorporal <bcorporal@gmail.com>
2022-03-30 23:35:14 -0400

    updated containers and background

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+$('#example1')
+  .awesomeCursor('paper-plane', {
+    color: '#2cb2da',
+    hotspot: 'top right'
+  });
+
